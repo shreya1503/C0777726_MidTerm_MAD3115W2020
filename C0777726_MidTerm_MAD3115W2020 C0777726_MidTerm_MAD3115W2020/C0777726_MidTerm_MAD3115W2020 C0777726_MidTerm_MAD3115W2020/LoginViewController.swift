f48e0aedd7c90f4308110461fc1c138968370237
shreya <shreyavaghasia99@gmail.com>
2020-03-06 18:53:22 -0500

    [SV] Alert Added To Login View Controller

--- C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/LoginViewController.swift
+++ C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/LoginViewController.swift
@@ -12,32 +12,35 @@ class LoginViewController: UIViewController
 {
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
-    
-    //@IBOutlet weak var lblRememberMe: UILabel!
-    @IBOutlet weak var switchRmbr: UISwitch!
+
+    //@IBOutlet weak var switchRmbr: UISwitch!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
               
-        RememberMe()
+       // RememberMe()
     }
 
-    //@IBAction func RememberMe(_ sender: UISwitch) {}
-    
-    func RememberMe()
+       
+    @IBAction func btnLogin(_ sender: UIBarButtonItem)
     {
-        let userDefault =  UserDefaults.standard
-        
-        if let email = userDefault.string(forKey: "email")
-        {
-            txtEmail.text = email
-            
-            if let pass = userDefault.string(forKey: "pass")
-            {
-                txtPassword.text = pass
-            }
-        }
+        let alert = UIAlertController(title: "ERROR", message: "Check Your Email ID Or Password", preferredStyle: .alert)
     }
+    
+    /* func RememberMe()
+         {
+             let userDefault =  UserDefaults.standard
+             
+             if let email = userDefault.string(forKey: "email")
+             {
+                 txtEmail.text = email
+                 
+                 if let pass = userDefault.string(forKey: "pass")
+                 {
+                     txtPassword.text = pass
+                 }
+             }
+         }*/
     
 }
