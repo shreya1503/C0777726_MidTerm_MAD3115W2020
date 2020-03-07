b5cfefe6cd4e1a5bc73f2e7cadb689fcbe139730
shreya <shreyavaghasia99@gmail.com>
2020-03-06 19:46:39 -0500

    [SV] Code To Switch Added For Remember Me

--- C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/LoginViewController.swift
+++ C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/LoginViewController.swift
@@ -13,6 +13,7 @@ class LoginViewController: UIViewController
     @IBOutlet weak var txtEmail: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
 
+    @IBOutlet weak var switchRemember: UISwitch!
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -20,25 +21,51 @@ class LoginViewController: UIViewController
        // RememberMe()
     }
 
-    
+    private func RememberMe()
+    {
+        let userDefault = UserDefaults.standard
+        
+        if let userName = userDefault.string(forKey: "Email")
+        {
+            txtEmail.text = userName
+            
+            if let pwd = userDefault.string(forKey: "Password")
+            {
+                txtPassword.text = pwd
+            }
+        }
+    }
        
     @IBAction func btnLogin(_ sender: UIBarButtonItem)
     {
-        let alertController = UIAlertController(title: "ERROR", message: "Check Your Email ID Or Password", preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "Invalid Email Or Password", style: .default	))
+        
+               if self.txtEmail.text == "Shreya" && self.txtPassword.text == "Shreya123"
+                {
+                    let userDefault = UserDefaults.standard
+                   if switchRemember.isOn
+                    {
+                        
+                        userDefault.setValue(txtEmail.text, forKey: "Email")
+                        userDefault.set(txtPassword.text, forKey: "Password")
+                    }
+                    else
+                    {
+                        userDefault.removeObject(forKey: "Email")
+                        userDefault.removeObject(forKey: "Password")
+                    }
+                }
+                else
+                {
+                    let alertController = UIAlertController(title: "ERROR", message: "Check Your Email ID Or Password", preferredStyle: .alert)
+                    alertController.addAction(UIAlertAction(title: "Invalid Email Or Password", style: .default))
 
-        self.present(alertController, animated: true, completion: nil)
-        
-        
-        if txtEmail.text == "admin" && txtPassword.text == "admin123"
-        {
-            print(txtEmail)
-        }
+                    self.present(alertController, animated: true, completion: nil)
+
+                }
     }
     
     @IBAction func unWindLogoutFromAnyScreen(storyboardSegue: UIStoryboardSegue)
     {
       print("Logout")
     }
-    
 }
