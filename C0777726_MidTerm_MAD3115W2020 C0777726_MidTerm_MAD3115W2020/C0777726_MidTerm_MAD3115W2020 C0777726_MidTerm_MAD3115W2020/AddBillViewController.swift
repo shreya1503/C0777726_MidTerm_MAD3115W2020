18c86fada8f75d06c2b23a90f05a39691051a26d
shreya1503 <shreyavaghasia99@gmail.com>
2020-03-13 18:21:02 -0400

    [SV] Conditions Added In Selected Segment Index

--- C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/AddBillViewController.swift
+++ C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/AddBillViewController.swift
@@ -17,9 +17,9 @@ class AddBillViewController: UIViewController
     @IBOutlet weak var txtBillID: UITextField!
     @IBOutlet weak var txtBillDate: UITextField!
     @IBOutlet weak var txtIProviderName: UITextField!
-    @IBOutlet weak var txtMobManufacturerName: UITextField!
     @IBOutlet weak var txtInternetGBUsed: UITextField!
     @IBOutlet weak var txtPlanName: UITextField!
+    @IBOutlet weak var txtMobManufacturerName: UITextField!
     @IBOutlet weak var txtMobileNumber: UITextField!
     @IBOutlet weak var txtMobileGBUsed: UITextField!
     @IBOutlet weak var txtMobileMinUsed: UITextField!
@@ -33,6 +33,49 @@ class AddBillViewController: UIViewController
         super.viewDidLoad()
         self.title = "Add New Bill"
     }
+    
+    @IBAction func segBillType(_ sender: UISegmentedControl)
+    {
+        
+        if sender.selectedSegmentIndex == 0
+        {
+            
+            txtIProviderName.isHidden = true
+            txtInternetGBUsed.isHidden = true
+            txtPlanName.isHidden = true
+            txtMobileGBUsed.isHidden = true
+            txtMobileGBUsed.isHidden = true
+            txtMobileNumber.isHidden = true
+            txtMobManufacturerName.isHidden = true
+            
+        }
+        
+        else if sender.selectedSegmentIndex == 1
+        {
+            
+            txtInternetGBUsed.isHidden = true
+            txtIProviderName.isHidden = true
+            txtHydroUnitsUsed.isHidden = true
+            txtHydroAgencyName.isHidden = true
+            
+        }
+        
+        else if sender.selectedSegmentIndex == 2
+        {
+            
+            txtHydroAgencyName.isHidden = true
+            txtHydroUnitsUsed.isHidden = true
+            txtPlanName.isHidden = true
+            txtMobileGBUsed.isHidden = true
+            txtMobileGBUsed.isHidden = true
+            txtMobileNumber.isHidden = true
+            txtMobManufacturerName.isHidden = true
+            
+            
+        }
+        
+    }
+    
     
     @IBAction func btnAddBill(_ sender: Any)
     {
