282dd9bae650586f03ad557e623914e820059b50
Rakesh Kumar <rakeshkumar92215@gmail.com>
2020-03-13 14:41:31 -0400

    [SV] New File Of Bill Table View Cell Added

---
+++ C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/BillTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BillTableViewCell.swift
+//  C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class BillTableViewCell: UITableViewCell
+{
+
+    
+
+}
--- C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/DetailedBillViewController.swift
+++ C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/DetailedBillViewController.swift
@@ -21,6 +21,7 @@ class DetailedBillViewController: UIViewController
     {
         super.viewDidLoad()
 
+  
      
     }
     
