af8245acb9e5fb85bb8af36625defa56abe06373
Shreya <shreyavaghasia99@gmail.com>
2020-03-12 21:11:39 -0400

    [SV] Swift File Created For Bill Calculation And Protocol Added For Same

--- C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/CalculateBill.swift
+++ C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/CalculateBill.swift
@@ -1,5 +1,5 @@
 //
-//  Bill.swift
+//  CalculateBill.swift
 //  C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020
 //
 //  Created by MacStudent on 2020-03-12.
@@ -8,4 +8,7 @@
 
 import Foundation
 
-
+protocol CalculateBill
+{
+    func calculateTotalBill() -> Double
+}
