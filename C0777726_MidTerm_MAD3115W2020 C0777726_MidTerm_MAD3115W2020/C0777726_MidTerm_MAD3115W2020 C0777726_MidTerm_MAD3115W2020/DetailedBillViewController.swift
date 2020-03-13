641c34163d411688ce890d66a35aac1a2f7b410a
Shreya <shreyavaghasia99@gmail.com>
2020-03-12 21:04:04 -0400

    [SV] New View Controller File Added For Detailed Bill

---
+++ C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/DetailedBillViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailedBillViewController.swift
+//  C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-12.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class DetailedBillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
