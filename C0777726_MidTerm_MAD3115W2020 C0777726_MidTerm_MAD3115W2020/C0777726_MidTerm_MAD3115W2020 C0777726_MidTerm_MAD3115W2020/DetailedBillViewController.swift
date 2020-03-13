619fdc097634fb89460ecbaaf430362b95d1dec6
Rakesh Kumar <rakeshkumar92215@gmail.com>
2020-03-13 14:33:19 -0400

    [SV] Outlet Of TableView And Label Created

--- C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/DetailedBillViewController.swift
+++ C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/DetailedBillViewController.swift
@@ -8,23 +8,21 @@
 
 import UIKit
 
-class DetailedBillViewController: UIViewController {
+class DetailedBillViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+@IBOutlet weak var lblTotalBill: UILabel!
+@IBOutlet weak var tbtBillDetail: UITableView!
+
+
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+     
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
