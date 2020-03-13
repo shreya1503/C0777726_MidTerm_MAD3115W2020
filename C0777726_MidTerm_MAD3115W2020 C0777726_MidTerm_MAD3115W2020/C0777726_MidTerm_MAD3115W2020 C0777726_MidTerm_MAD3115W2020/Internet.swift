319835a54ac938ec0aad602c726fb0ff9186d441
Shreya <shreyavaghasia99@gmail.com>
2020-03-12 21:28:03 -0400

    [SV] Internet Class Created And Function Added To Calculate Bill

--- C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/Internet.swift
+++ C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/Internet.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class Internet: Bill
+{
+    var providerName: String
+    var internetGBUsed: Double
+    var ratePerGB: Double = 7.5
+
+    init(billID: String, billDate : Date, billType : BillType, providerName : String, internetGBUsed : Double)
+    {
+        self.providerName = providerName
+        self.internetGBUsed = internetGBUsed
+        super.init(billID : billID, billDate : billDate, billType : billType)
+        self.billAmount = calculateTotalBill()
+    }
+
+    override func calculateTotalBill() -> Double
+    {
+        self.billAmount = internetGBUsed * ratePerGB
+        return billAmount
+    }
+}
