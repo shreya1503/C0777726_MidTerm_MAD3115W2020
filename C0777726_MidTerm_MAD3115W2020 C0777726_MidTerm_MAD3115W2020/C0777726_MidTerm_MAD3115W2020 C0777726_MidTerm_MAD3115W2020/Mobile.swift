9a7b0f611071b574284e27e116d938e71eb40810
Shreya <shreyavaghasia99@gmail.com>
2020-03-12 21:22:10 -0400

    [SV] Initialized value in Mobile Class

--- C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/Mobile.swift
+++ C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/Mobile.swift
@@ -10,12 +10,23 @@ import Foundation
 
 class Mobile: Bill
 {
-    var mobileManufacturerName: String = ""
-    var planName: String = ""
-    var mobileNumber: String = ""
-    var internetGBUsed: Double = 0.0
-    var minuteUsed: Int = 0
-    var ratePerGB: Double = 7.5
-    var ratePerMinute: Double = 0.50
+    var mobileManufacturerName : String
+    var planName : String
+    var mobileNumber : String
+    var internetGBUsed : Double
+    var minuteUsed : Int
+    var ratePerGB : Double = 5.5
+    var ratePerMinute : Double = 0.50
     
+
+   init(billID : String, billDate : Date, billType : BillType, mobileManufacturerName : String, planName : String, mobileNumber : String, internetGBUsed : Double, minuteUsed : Int)
+   {
+       self.mobileManufacturerName=mobileManufacturerName
+       self.planName=planName
+       self.mobileNumber=mobileNumber
+       self.internetGBUsed=internetGBUsed
+       self.minuteUsed=minuteUsed
+       super.init(billID: billID, billDate: billDate, billType: billType)
+       self.billAmount = calculateTotalBill()
+   }
 }
