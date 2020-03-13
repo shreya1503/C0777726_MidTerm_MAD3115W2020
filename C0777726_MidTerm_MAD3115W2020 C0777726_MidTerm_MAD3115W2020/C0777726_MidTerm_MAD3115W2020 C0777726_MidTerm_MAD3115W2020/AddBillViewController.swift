eaab8cc3b5b0f410f17519fa97fdd3f71722c393
Rakesh Kumar <rakeshkumar92215@gmail.com>
2020-03-13 14:38:35 -0400

    [SV] New View Controller Added To Insert New Bill

---
+++ C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020/AddBillViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddBillViewController.swift
+//  C0777726_MidTerm_MAD3115W2020 C0777726_MidTerm_MAD3115W2020
+//
+//  Created by MacStudent on 2020-03-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class AddBillViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
